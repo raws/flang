157d7fcd7924ee7d35b1bf9c4c0e206d48483fea
Ross Paffett <ross@rosspaffett.com>
2011-10-15 02:13:35 -0400

    Check for existing ExpressionEngine login session if user is not already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :logged_in?, :logged_out?
+  before_filter :check_for_session_cookie, lambda { logged_out? }
   
   def current_user
     @current_user ||= User.find_by_member_id(session[:user]) if logged_in?
@@ -13,4 +14,13 @@ class ApplicationController < ActionController::Base
   def logged_out?
     !logged_in?
   end
+  
+  private
+    def check_for_session_cookie
+      if session_id = cookies[:exp_sessionid]
+        if ee_session = Session.find_by_session_id(session_id)
+          session[:user] = ee_session.member_id
+        end
+      end
+    end
 end

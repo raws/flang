26a13b2ae894be793a6fe0980e89bdd83265187a
Ross Paffett <ross@rosspaffett.com>
2011-10-22 22:13:01 -0400

    Don't check for session cookie when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user, :logged_in?, :logged_out?
-  before_filter :check_for_session_cookie, lambda { logged_out? }
+  before_filter :check_for_session_cookie
   
   def current_user
     @current_user ||= User.find_by_member_id(session[:user]) if logged_in?
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::Base
   
   private
     def check_for_session_cookie
+      return if logged_in?
       if session_id = cookies[:exp_sessionid]
         if ee_session = Session.find_by_session_id(session_id)
           session[:user] = ee_session.member_id

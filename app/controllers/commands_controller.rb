d9d3cddc11770df430c1bd8f3f6bfdbb6a7453c0
Ross Paffett <ross@rosspaffett.com>
2011-10-23 00:41:40 -0400

    Add ability to update commands

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -4,11 +4,11 @@ class CommandsController < ApplicationController
   def index
     @commands = Command.all(:order => :name)
   end
-
+  
   def new
     @command = Command.new
   end
-
+  
   def create
     @command = Command.new(params[:command])
     @command.user_id = current_user.member_id
@@ -22,14 +22,23 @@ class CommandsController < ApplicationController
   def show
     @command = Command.find_by_id(params[:id])
   end
-
+  
   def edit
     @command = Command.find_by_id(params[:id])
   end
-
+  
   def update
+    if @command = Command.find_by_id(params[:id])
+      if @command.update_attributes(params[:command])
+        redirect_to @command
+      else
+        render :action => "edit"
+      end
+    else
+      redirect_to commands_path
+    end
   end
-
+  
   def destroy
   end
 end
